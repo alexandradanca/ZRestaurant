fcf2c3b6d543cfb18f7d6d237a867ecc71c07016
Danca Alexandra <alexandra_simona_danca@yahoo.com>
2024-09-23 21:35:15 +0300

    update navbar effect

--- js/navbar.js
+++ js/navbar.js
@@ -1,8 +1,27 @@
-const btn_click = document.querySelector('.expand-nav');
+const icon_click = document.querySelector('.expand-nav');
 const menuList = document.querySelector('.menu-container>ul');
+const bts = document.querySelectorAll('.menu-container>ul>li>a');
 
-btn_click.addEventListener('click', ()=>{
+icon_click.addEventListener('click', ()=>{
     menuList.classList.toggle('menu-active');
-    btn_click.classList.toggle('btn_clicked');
+    icon_click.classList.toggle('btn_clicked');
+    
 });
 
+
+bts.forEach((x)=>{
+    x.addEventListener('click', (y)=>{
+
+        bts.forEach(btnX => btnX.classList.remove('active'));
+
+        y.currentTarget.classList.toggle('active');
+       
+        if(icon_click.classList.contains('btn_clicked')){
+            console.log('Activ');
+            if(x.classList.contains('active')){
+                menuList.classList.toggle('menu-active');
+                icon_click.classList.toggle('btn_clicked');
+            }
+        }     
+    });
+});
